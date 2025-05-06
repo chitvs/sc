4ad85083c5c45e98b7e19a476d48f8cc793c1bd0
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-06 10:32:36 +0200

    e1A [06-2019]

--- src/24-06-2019/A/e1/e1A.s
+++ src/24-06-2019/A/e1/e1A.s
@@ -1,1 +1,35 @@
-# inserire la soluzione qui...
+.globl init_matrix
+
+init_matrix:
+    pushl %esi
+    pushl %edi
+    pushl %ebx
+    pushl %ebp
+    subl $8, %esp
+    movl 28(%esp), %esi
+    movl 32(%esp), %edi
+    movl $0, %ebx
+F1:
+    cmpl %edi, %ebx
+    jae E
+    movl $0, %ebp
+F2:
+    cmpl %edi, %ebp
+    jae NEXT
+    movl %ebx, (%esp)
+    movl %ebp, 4(%esp)
+    call value
+    movl (%esi,%ebx,4), %ecx
+    movw %ax, (%ecx,%ebp,2)
+    incl %ebp
+    jmp F2      
+NEXT:
+    incl %ebx
+    jmp F1
+E:
+    addl $8, %esp
+    popl %ebp
+    popl %ebx
+    popl %edi 
+    popl %esi 
+    ret
--- src/24-06-2019/A/e1/e1A_eq.c
+++ src/24-06-2019/A/e1/e1A_eq.c
@@ -1,1 +1,21 @@
-// inserire il C equivalente qui...
+#include "e1A.h"
+
+void init_matrix(short** m, unsigned n) {
+    unsigned i = 0;
+    unsigned j;
+F1:
+    if (i >= n) goto E;
+    j = 0;
+F2:
+    if (j >= n) goto NEXT;
+    short tmp = value(i,j);
+    short* tmp2 = m[i];
+    tmp2[j] = tmp;
+    ++j;
+    goto F2;
+NEXT:
+    ++i;
+    goto F1;
+E:
+    return;
+}
