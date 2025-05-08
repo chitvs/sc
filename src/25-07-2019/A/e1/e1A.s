fac10a6dd1c1ae92836f381c5b4423095aa9d032
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-08 09:58:58 +0200

    update e1A [07-2019]

--- src/25-07-2019/A/e1/e1A.s
+++ src/25-07-2019/A/e1/e1A.s
@@ -1,38 +1,35 @@
 .globl most_freq_char
 
 most_freq_char:
-    pushl %ebx #s
-    pushl %edi #map
-    subl $8, (%esp)
-    movl 20(%esp), %ebx
-    movl 34(%esp), %edi
-    movl $0, %eax
-    movl $0, %ecx
+    pushl %esi
+    pushl %edi
+    subl $8, %esp
+    movl 20(%esp), %esi
+    movl 24(%esp), %edi
     movl %edi, (%esp)
     movl $256, 4(%esp)
     call clear
-L:  
-    movl (%ebx), %edx
-    cmpl $0, %edx
-    jnz E1
-    incl (%edi, %edx, 4)
-    incl %ebx
-    jmp L
-E1:
-    cmpl $256, %esi
-    jge E2
-    movl (%edi, %ebp, 4), %ecx
-    cmpl %ecx, (%edi, %esi, 4)
-    jle NEXT
-    movl %esi, %ebp
-NEXT:
+    xorl %eax, %eax
+L1:
+    movb (%esi), %dl
+    movsbl %dl, %edx
+    testl %edx, %edx
+    je E1
+    incl (%edi,%edx,4)
     incl %esi
-    jmp E1
+    jmp L1
+E1:
+    xorl %ecx, %ecx
+L2:
+    cmpl $256, %ecx
+    jge E2
+    movl (%edi,%ecx,4), %edx
+    cmpl (%edi,%eax,4), %edx
+    cmovgl %ecx, %eax
+    incl %ecx
+    jmp L2
 E2:
-    movl %ebp, %eax
-    addl $8, (%esp)
-    popl %ebp
+    addl $8, %esp
+    popl %edi
     popl %esi
-    popl %edi 
-    popl %ebx
     ret
