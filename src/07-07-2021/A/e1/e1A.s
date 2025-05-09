1438a9ff50c2afa5d94906c0549a13e2b2f4b819
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-09 15:08:13 +0200

    e1A [07-2021]

--- src/07-07-2021/A/e1/e1A.s
+++ src/07-07-2021/A/e1/e1A.s
@@ -1,1 +1,42 @@
-# inserire la soluzione qui...
+.globl suffix
+
+suffix:
+    pushl %ebx
+    pushl %ebp
+    pushl %esi 
+    pushl %edi
+    subl $4, %esp
+    movl 24(%esp), %ebx
+    movl 28(%esp), %ebp
+    movl %ebx, (%esp)
+    call strlen
+    movl %eax, %esi
+    movl %ebp, (%esp)
+    call strlen
+    movl %eax, %edi
+    cmpl %esi, %edi
+    jg E1
+    movl %esi, %ecx 
+    subl %edi, %ecx
+L:
+    cmpl %esi, %ecx 
+    jge E2
+    movb (%ebp), %dl
+    cmpb (%ebx, %ecx), %dl
+    jne E1
+    incl %ebp
+    incl %ecx 
+    jmp L
+E1:
+    movl $0, %eax
+    jmp E3
+E2:
+    movl $1, %eax
+    jmp E3
+E3:
+    addl $4, %esp
+    popl %edi
+    popl %esi
+    popl %ebp
+    popl %ebx
+    ret
--- src/07-07-2021/A/e1/e1A_eq.c
+++ src/07-07-2021/A/e1/e1A_eq.c
@@ -1,1 +1,29 @@
-// inserire il C equivalente qui...
+#include "e1A.h"
+
+int suffix(const char* a, const char* b) {
+    const char* ebx = a;
+    const char* ebp = b;
+    int eax = strlen(ebx); 
+    int esi = eax; // alen
+    eax = strlen(ebp);
+    int edi = eax; // blen
+    int ecx; // i
+    int edx; // *b++
+    if (edi > esi)
+        goto E1;
+    ecx = esi-edi;
+L:
+    if (ecx >= esi) goto E2;
+    edx = *ebp++;
+    if (ebx[ecx] != edx) goto E1;
+    ecx++;
+    goto L;
+E1:
+    eax = 0;
+    goto E3;
+E2:
+    eax = 1;
+    goto E3;
+E3:
+    return eax;
+}
