b03b8673ee14a717c5fcc9b62ba5777bcbf68b1e
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-13 18:37:33 +0200

    e1B [06-2023]

--- src/2023/06/B/e1/e1B.s
+++ src/2023/06/B/e1/e1B.s
@@ -1,1 +1,40 @@
-# Inserire qui la soluzione...
+.globl rc4_encrypt_rev
+
+rc4_encrypt_rev:
+    pushl %esi
+    pushl %ebx
+    pushl %edi 
+    pushl %ebp
+    subl $12, %esp
+    xorl %esi, %esi
+    xorb %bl, %bl
+    movl 36(%esp), %ebp
+    movl 40(%esp), %edi
+    movl %ebp, (%esp)
+    call strlen
+    addl %eax, %ebp
+    decl %ebp
+L:
+    cmpl 36(%esp), %ebp
+    jl E
+    incl %esi
+    andl $255, %esi
+    movl 32(%esp), %eax
+    addb (%eax, %esi, 1), %bl 
+    movl %eax, (%esp)
+    movl %esi, 4(%esp)
+    movb %bl, 8(%esp)
+    call rc4_helper
+    movb (%ebp), %ah
+    movb %al, (%edi)
+    xorb %ah, (%edi)
+    incl %edi
+    decl %ebp
+    jmp L
+E:
+    addl $12, %esp
+    popl %ebp
+    popl %edi
+    popl %ebx
+    popl %esi
+    ret
--- src/2023/06/B/e1/e1B_eq.c
+++ src/2023/06/B/e1/e1B_eq.c
@@ -1,3 +1,23 @@
 #include "e1B.h"
 
-// Inserire qui il C equivalente...
+void rc4_encrypt_rev(unsigned char *sbox, unsigned char *pt, unsigned char *ct)
+{
+unsigned int esi = 0; // i
+unsigned char ebx = 0; // j
+unsigned char *ebp = pt + strlen(pt) - 1;
+unsigned char *edi = ct;
+L:
+    if (ebp < pt)
+        goto E;
+        esi++;
+        esi &= 255;
+        unsigned char *eax = sbox;
+        ebx += eax[esi];
+        unsigned char al = rc4_helper(eax, esi, ebx) ^ *ebp;
+        edi = al;
+        edi++;  
+        ebp--;
+        goto L;
+E:
+    return;
+}
