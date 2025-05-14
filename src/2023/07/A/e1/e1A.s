fa6d37cbe867d6720cff8ea08fff8149ab1323d0
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-14 10:52:02 +0200

    e1A [07-2023]

--- src/2023/07/A/e1/e1A.s
+++ src/2023/07/A/e1/e1A.s
@@ -1,1 +1,51 @@
-# Inserire qui la soluzione...
+.globl adler32
+
+adler32:
+    pushl %ebx
+    pushl %ebp
+    pushl %edi 
+    pushl %esi
+    subl $4, %esp
+    movl 24(%esp), %ebx
+    testl %ebx, %ebx
+    je E1
+    cmpl $0, 28(%esp)
+    je E1
+    movl $1, %edi 
+    movl $0, %esi 
+    leal (%esp), %ebp
+    movl %ebp, (%esp)
+    call get_adler_constant
+    movl (%esp), %ebp
+    movl $0, %ecx
+L:
+    cmpl 28(%esp), %ecx
+    jge E2
+    movl %edi, %eax
+    movzbl (%ebx, %ecx, 1), %edx
+    addl %edx, %eax
+    xorl %edx, %edx
+    divl %ebp
+    movl %edx, %edi 
+    movl %esi, %eax 
+    addl %edi, %eax 
+    xorl %edx, %edx
+    divl %ebp
+    movl %edx, %esi 
+    incl %ecx
+    jmp L
+E1:
+    movl $0, %eax
+    jmp RET
+E2:
+    sall $16, %esi 
+    orl %edi, %esi 
+    movl %esi, %eax 
+    jmp RET
+RET:
+    addl $4, %esp
+    popl %esi
+    popl %edi 
+    popl %ebp
+    popl %ebx
+    ret
--- src/2023/07/A/e1/e1A_eq.c
+++ src/2023/07/A/e1/e1A_eq.c
@@ -1,3 +1,38 @@
 #include "e1A.h"
 
-// Inserire qui il C equivalente...
+unsigned adler32(unsigned char *data, unsigned len) {
+    unsigned char *ebx = data;
+    if (data == NULL)
+        goto E1;
+    if (len == 0)
+        goto E1;
+    unsigned edi = 1, esi = 0;
+    unsigned ebp;
+    get_adler_constant(&ebp);
+    int ecx = 0;
+    unsigned eax, edx;
+L:
+    if (ecx >= len)
+        goto E2;
+    eax = edi;
+    eax += ebx[ecx];
+    eax %= ebp;
+    eax = edx;
+    edi = edx;
+    edx = esi;
+    edx += edi;
+    edx %= ebp;
+    esi = edx;
+    ecx++;
+    goto L;
+E1:
+    eax = 0;
+    goto RET;
+E2:
+    esi = esi << 16;
+    esi = esi | edi;
+    eax = esi;
+    goto RET;
+RET:
+    return eax;
+}
