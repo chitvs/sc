125bc89979f90b1467034ba8a5c8e10d3c2664f1
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-18 01:11:48 +0200

    e1A [09-2023]

--- src/2023/09/A/e1/e1A.s
+++ src/2023/09/A/e1/e1A.s
@@ -1,1 +1,58 @@
-# Inserire qui la soluzione...
+.globl base64lessless
+
+base64lessless:
+    pushl %ebx
+    pushl %edi
+    pushl %esi
+    pushl %ebp
+    subl $4, %esp
+    cmpl $0, 24(%esp)
+    je E
+    cmpl $0, 28(%esp)
+    je E
+    movl 24(%esp), %esi
+    movl 28(%esp), %eax
+    movl %eax, (%esp)
+    call malloc
+    xorb %dl, %dl
+    xorb %dh, %dh
+    xorl %edi, %edi
+L:
+    cmpl 28(%esp), %edi
+    jge E
+    movb $6, %cl
+    subb %dl, %cl
+    movb %dh, %bl
+    shlb %cl, %bl
+    movb %dl, %cl
+    movb (%esi), %ch
+    shrb %cl, %ch
+    shrb $2, %ch
+    orb %ch, %bl
+    movb $8, %cl
+    subb $6, %cl
+    addb %dl, %cl
+    andb $7, %cl
+    movb %cl, %dl
+    movb $8, %cl
+    subb %dl, %cl
+    movb (%esi), %dh
+    shlb %cl, %dh
+    shrb %cl, %dh
+    cmpb $6, %dl
+    jae L2
+    incl %esi
+L2:
+    movl 32(%esp), %ecx
+    movzbl %bl, %ebx
+    movb (%ecx, %ebx), %cl
+    movb %cl, (%eax, %edi)
+    incl %edi
+    jmp L
+E:
+    addl $4, %esp
+    popl %ebp
+    popl %esi
+    popl %edi
+    popl %ebx
+    ret
--- src/2023/09/A/e1/e1A_eq.c
+++ src/2023/09/A/e1/e1A_eq.c
@@ -1,3 +1,43 @@
 #include "e1A.h"
 
-// Inserire qui il C equivalente...
+unsigned char* base64lessless(unsigned char *data, unsigned out_len, unsigned char* b64_table) {
+    unsigned char* eax = NULL;
+    unsigned char* esi = data;
+    if (esi == NULL) goto E;
+    if(out_len == 0) goto E;
+    eax = malloc(out_len);
+    unsigned char dl = 0; 
+    unsigned char dh = 0; 
+    unsigned char cl, ch; 
+    int edi = 0; 
+L:
+    if (edi >= out_len) goto E;
+    cl = 6;
+    cl -= dl;
+    unsigned char bl = dh; 
+    bl = bl << cl;
+    cl = dl;
+    ch = *esi;
+    ch = ch >> cl;
+    ch = ch >> 2;
+    bl = bl | ch;
+    cl = 8;
+    cl = cl - 6;
+    cl = cl + dl;
+    cl = cl & 7;
+    dl = cl;
+    cl = 8;
+    cl = cl - dl;
+    dh = *esi;
+    dh = dh << cl;
+    dh = dh >> cl;
+    if (dl >= 6) goto F;
+    esi += 1;
+F:
+    unsigned ebx = bl;
+    eax[edi] = b64_table[ebx];
+    edi += 1;
+    goto L;
+E:
+    return eax;
+}
