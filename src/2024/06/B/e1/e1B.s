0f53471a0791ff8883c30bfcccfb6fdf3629c437
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-18 13:59:23 +0200

    e1B [06-2024]

--- src/2024/06/B/e1/e1B.s
+++ src/2024/06/B/e1/e1B.s
@@ -1,1 +1,35 @@
-# Inserire la soluzione qui
+.globl deleetify
+
+deleetify:
+    pushl %edi 
+    pushl %ebx
+    pushl %esi
+    subl $8, %esp
+    xorl %esi, %esi
+    movl 24(%esp), %ebx
+    movl 32(%esp), %edi
+L:
+    cmpl 28(%esp), %esi
+    jae RET
+    cmpb $57, (%ebx, %esi, 1)
+    ja L2
+    movb (%ebx, %esi, 1), %cl
+    movb %cl, (%esp)
+    leal 4(%esp), %eax
+    movl %eax, 4(%esp)
+    call deleetify_helper
+    movb 4(%esp), %al
+    movb %al, (%edi, %esi, 1)
+    incl %esi
+    jmp L
+L2:
+    movb (%ebx, %esi, 1), %cl
+    movb %cl, (%edi, %esi, 1)
+    incl %esi
+    jmp L
+RET:
+    addl $8, %esp
+    popl %esi
+    popl %ebx
+    popl %edi
+    ret
--- src/2024/06/B/e1/e1B_eq.c
+++ src/2024/06/B/e1/e1B_eq.c
@@ -1,1 +1,25 @@
-// Inserire il C equivalente qui (opzionale: non concorre alla valutazione)
+#include "e1B.h"
+
+void deleetify(unsigned char* v, unsigned len, unsigned char* res)
+{
+    unsigned esi = 0;
+    unsigned char al;
+    unsigned char* ebx = v;
+    unsigned char* edi = res;
+    unsigned char cl;
+L:
+    if (esi >= len)
+        goto RET;
+    if (ebx[esi] > 57) goto L2;
+    cl = ebx[esi];
+    deleetify_helper(cl, &al);
+    edi[esi] = al;
+    esi++;
+    goto L;
+L2:
+    cl = ebx[esi];
+    edi[esi] = cl;
+    esi++;
+    goto L;
+RET:;
+}
