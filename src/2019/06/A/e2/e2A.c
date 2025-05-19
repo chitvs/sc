2668ef4cc64c2b75fb5d96d268ffb81b76101397
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-19 11:35:52 +0200

    e2A [06-2019]

--- src/2019/06/A/e2/e2A.c
+++ src/2019/06/A/e2/e2A.c
@@ -1,4 +1,23 @@
 #include "e2A.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-// inserisci la soluzione qui...
-
+void run(int* v, int n, int (*f)(int i)){
+    int ex;
+    for (int i=0; i<n; i++){
+        pid_t pid = fork();
+        if (pid == -1) {
+            exit(EXIT_FAILURE);
+        }
+        if (pid == 0) {
+            ex = f(i);
+            exit(ex);
+        }
+        int status;
+        pid = wait(&status);
+        if (WIFEXITED(status))
+            v[i] = WEXITSTATUS(status);
+    }
+}
