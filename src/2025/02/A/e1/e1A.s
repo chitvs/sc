4801db619d0c4690721b57b7ddd8d8e9124a8ba2
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-19 10:53:58 +0200

    e1A [02-2025]

--- src/2025/02/A/e1/e1A.s
+++ src/2025/02/A/e1/e1A.s
@@ -1,1 +1,30 @@
-# inserire la soluzione qui
+.globl count_tokens
+
+count_tokens:
+    pushl %edi 
+    pushl %ebx 
+    pushl %esi 
+    subl $8, %esp
+    xorl %edi, %edi 
+    movl 28(%esp), %esi
+    movl 24(%esp), %eax
+    movl %eax, (%esp)
+    movl %esi, 4(%esp)
+    call strtok
+    movl %eax, %ebx
+L:
+    cmpl $0, %ebx
+    je RET
+    incl %edi 
+    movl $0, (%esp)
+    movl %esi, 4(%esp)
+    call strtok
+    movl %eax, %ebx
+    jmp L
+RET:
+    movl %edi, %eax
+    addl $8, %esp
+    popl %esi 
+    popl %ebx 
+    popl %edi 
+    ret
--- src/2025/02/A/e1/e1A_eq.c
+++ src/2025/02/A/e1/e1A_eq.c
@@ -1,1 +1,20 @@
-// Inserire il C equivalente qui (opzionale: non concorre alla valutazione)
+#include "e1A.h"
+
+int count_tokens(char* str, const char* sep) {
+    int edi = 0;
+    int eax;
+    const char* esi = sep;
+    char* tmp = str;
+    char* a = strtok (tmp, esi);
+    char* ebx = a;
+L:
+    if (ebx == 0)
+        goto RET;
+    edi++;
+    a = strtok(0, esi);
+    ebx = a;
+    goto L;
+RET:
+    eax = edi;
+    return eax;
+}
