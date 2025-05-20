614f02edabd077218bcf99f4dea96c82b0e13a50
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-20 17:12:59 +0200

    e2B [06-2021]

--- src/2021/06/B/e2/e2B.c
+++ src/2021/06/B/e2/e2B.c
@@ -1,3 +1,32 @@
 #include "e2B.h"
 
-// inserisci la soluzione qui...
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+off_t searchfile(char* filename, char c){
+    int file = open(filename, O_RDONLY);
+    if (file == -1){
+        perror("errore nell'aprire il file");
+        exit(EXIT_FAILURE);
+    }
+    off_t offset = 0;
+    char buffer;
+    ssize_t res = read(file, &buffer, 1);
+    while (res > 0) {
+        if (buffer == c) {
+            close(file);
+            return offset;
+        }
+        offset++;
+        res = read(file, &buffer, 1);
+    }
+    if (res == -1) {
+        perror("errore in read");
+        close(file);
+        exit(EXIT_FAILURE);
+    }
+    close(file);
+    return -1;
+}
