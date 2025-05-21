9456bd01dd50cd34396a21137155e69e936bdc1e
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-21 21:57:25 +0200

    e2A [07-2023]

--- src/2023/07/A/e2/e2A.c
+++ src/2023/07/A/e2/e2A.c
@@ -1,5 +1,24 @@
 #include "e2A.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int check_vocals(char c){
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+        return 1;
+    return 0;
+}
+
 int vowelcount(const char** s, int n){
-    
+    if (n == 0)
+        return -1;
+    int cnt = 0;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < strlen(s[i]); j++){
+            if (check_vocals(s[i][j]))
+                cnt++;
+        }
+    }
+    return cnt;
 }
