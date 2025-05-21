19376f1ba6c88be46953a08777d2f392e8aba1df
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-21 21:37:27 +0200

    e2B [06-2023]

--- src/2023/06/B/e2/e2B.c
+++ src/2023/06/B/e2/e2B.c
@@ -1,4 +1,37 @@
 #include "e2B.h"
 
-// inserisci la soluzione qui...
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int getBalanceMovements(const char* fname, int min, int max, int *bal){
+    FILE *f = fopen(fname, "r");
+
+    char line[15];
+
+    int cnt = 0;
+    int bal_value = 0;
+
+    if (!f) return -1;
+
+    while (fgets(line, sizeof(line), f)){
+        char *day = strtok(line, "|");
+        char *pm = strtok(NULL, "|");
+        char *value = strtok(NULL, "|");
+
+        if (atoi(day) >= min && atoi(day) <= max){
+            cnt++;
+            if (pm[0] == '+')
+                bal_value += atoi(value);
+            else
+                bal_value -= atoi(value);
+        }
+    }
+
+    *bal = bal_value;
+
+    fclose(f);
+
+    return cnt;
+
+}
