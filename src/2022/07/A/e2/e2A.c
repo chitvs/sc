e47c68287823db031c45afc17b8bef87417412c1
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-21 14:51:14 +0200

    e2A [07-2022]

--- src/2022/07/A/e2/e2A.c
+++ src/2022/07/A/e2/e2A.c
@@ -1,3 +1,55 @@
 #include "e2A.h"
 
-// inserisci la soluzione qui...
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void getLargeTables(struct booking **list, const char *filename, int num) {
+    struct booking *head = NULL;
+    struct booking *tail = NULL;
+
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    char surname[31] ;
+    char places[3];
+    char time[6];
+
+    while (
+        fread(surname, 1, 30, f) == 30 &&
+        fread(places, 1, 2, f) == 2 &&
+        fread(time, 1, 5, f) == 5
+    ) {
+        for (int i=0;i<30;i++){
+            if (surname[i] == '_')
+                surname[i] = '\0';
+        }
+        if (atoi(places) >= num) {
+            struct booking *new_node = malloc(sizeof(struct booking));
+            
+            if (!new_node) {
+                perror("malloc failed");
+                fclose(f);
+                return;
+            }
+
+            new_node->surname = strdup(surname);  
+            new_node->time = strdup(time);        
+            new_node->places = atoi(places);
+            new_node->next = NULL;
+
+            if (head == NULL) {
+                head = tail = new_node;
+            } else {
+                tail->next = new_node;
+                tail = new_node;
+            }
+        }
+    }
+
+    fclose(f);
+    *list = head;
+}
