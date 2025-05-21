4afe65ea30378eca107224e7e90bc96517215354
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-21 21:23:27 +0200

    e2A [06-2023]

--- src/2023/06/A/e2/e2A.c
+++ src/2023/06/A/e2/e2A.c
@@ -1,37 +1,49 @@
 #include "e2A.h"
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <string.h>
 
-int examStats(const char* fname, int * min, int * max, float * avg){
-    char line[256];
-
-    int grades = 0;
-    int maxx = 0;
-    int minn = 30;
-    int cnt = 0;
-    
+int examStats(const char* fname, int *min, int *max, float *avg) {
     FILE *f = fopen(fname, "r");
-
     if (!f) return -1;
 
-    while (fgets(line, sizeof(line), f)){
-        char* surname = strtok(line, "-");
-        char* name = strtok(NULL, "-");
-        char* grade = strtok(NULL, "-");
-        if (atoi(grade) >= 18){
-            grades += atoi(grade);
-            cnt++;
+    char line[256];
+    int passedCount = 0;
+    float sum = 0;
+
+    int localMin = 31;
+    int localMax = 0;
+
+    while (fgets(line, sizeof(line), f)) {
+        char *surname = strtok(line, "-");
+        char *name = strtok(NULL, "-");
+        char *gradeStr = strtok(NULL, "-");
+
+        if (!surname || !name || !gradeStr)
+            continue;
+
+        int grade = atoi(gradeStr);
+
+        if (grade >= 18) {
+            if (grade < localMin) localMin = grade;
+            if (grade > localMax) localMax = grade;
+            sum += grade;
+            passedCount++;
         }
-        if (atoi(grade) > maxx)
-            maxx = atoi(grade);
-        if (atoi(grade) < minn)
-            minn = atoi(grade);
-        *min = minn;
-        *max = maxx;
-        *avg = grades/cnt;
     }
 
-    return cnt;
+    fclose(f);
+
+    if (passedCount == 0) {
+        *min = 0;
+        *max = 0;
+        *avg = 0.0;
+        return 0;
+    }
+
+    *min = localMin;
+    *max = localMax;
+    *avg = sum / passedCount;
+    return passedCount;
 }
