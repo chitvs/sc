bfa31548a3364ee376fda651d64653b43731019a
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-21 22:02:00 +0200

    e2A [07-2023]

--- src/2023/07/A/e2/e2A.c
+++ src/2023/07/A/e2/e2A.c
@@ -1,24 +1,53 @@
 #include "e2A.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
+#include <ctype.h>
 
-int check_vocals(char c){
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-        return 1;
-    return 0;
+void check_error(int res, char* msg) {
+    if (res != -1) return;
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
-int vowelcount(const char** s, int n){
-    if (n == 0)
+int vowelcount(const char** s, int n) {
+
+    if (s == NULL || n == 0) {
         return -1;
-    int cnt = 0;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < strlen(s[i]); j++){
-            if (check_vocals(s[i][j]))
-                cnt++;
+    }
+
+    int totalVowels = 0;
+
+    for (int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        check_error(pid,"Error in Fork");
+        if (pid == 0) {
+            const char* str = s[i];
+            int vowels = 0;
+            int len = strlen(str);
+            
+            for (int j = 0; j < len; j++) {
+                char ch = tolower(str[j]);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                }
+            }
+            
+            exit(vowels);
         }
     }
-    return cnt;
+
+    for (int i = 0; i < n; i++) {
+        int status;
+	    while(wait(&status) > 0){
+			if(WIFEXITED(status)){
+				totalVowels += WEXITSTATUS(status);
+			}
+	    }
+    }
+
+    return totalVowels;
 }
