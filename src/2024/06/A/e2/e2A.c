d22814864d37e608417a4469f2e845c3a37181dc
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-22 15:08:06 +0200

    add files [06-2024]

---
+++ src/2024/06/A/e2/e2A.c
@@ -0,0 +1,10 @@
+#include "e2A.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void destinazioniDisponibili(const char * filename, const char *
+partenza, int budget, struct destinazione ** list){
+    
+}
