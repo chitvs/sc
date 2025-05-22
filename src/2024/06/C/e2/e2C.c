bb934c63833086ce1c22a550e08e134c7a66354d
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-22 23:23:15 +0200

    e2C [06-2024]

--- src/2024/06/C/e2/e2C.c
+++ src/2024/06/C/e2/e2C.c
@@ -3,20 +3,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct destinazione {
+        char* dep;
+        char* arr;
+        int cost;
+};
+
+int compare(const void *a, const void *b){
+    struct destinazione *ca = *(struct destinazione **)a;
+    struct destinazione *cb = *(struct destinazione **)b;
+    return ca->cost - cb->cost;
+}
+
 void destinazioniSottoBudget(const char * filesorgente, const char *partenza, int budget, const char * filedestinazione){
     
     FILE *f1 = fopen(filesorgente, "r");
     FILE *f2 = fopen(filedestinazione, "w");
 
-    char s[100];
+    int count = 0;
+
+    struct destinazione *array[100];
+    char s[15];
 
     while (fgets(s, sizeof(s), f1)){
         char* dep = strtok(s, "-");
         char* arr = strtok(NULL, "-");
         char* cost = strtok(NULL, "-");
+
         if (atoi(cost) <= budget && strcmp(dep, partenza) == 0){
-            fprintf(f2, "%s-%s-%s\n", dep, arr, cost);
+            
+            struct destinazione *x = malloc(sizeof(struct destinazione));
+            x->dep = strdup(dep);
+            x->arr = strdup(arr);
+            x->cost = atoi(cost);
+            array[count] = x;
+            count++;
         }
+    }
+
+    qsort(array, count, sizeof(struct destinazione*), compare);
+
+    for (int i = 0; i < count; i++){
+        fprintf(f2, "%s-%s-%d\n", array[i]->dep, array[i]->arr, array[i]->cost);
     }
 
     fclose(f1);
