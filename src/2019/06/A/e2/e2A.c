1370a64837e59181042f187952c72ac949f61d39
Alessandro <alessandro.chitarrini51@gmail.com>
2025-05-26 12:36:45 +0200

    update e2A [06-2019]

--- src/2019/06/A/e2/e2A.c
+++ src/2019/06/A/e2/e2A.c
@@ -1,23 +1,32 @@
 #include "e2A.h"
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void check_error(int res, char* msg){
+    if (res!=-1) return;
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
 void run(int* v, int n, int (*f)(int i)){
-    int ex;
+
     for (int i=0; i<n; i++){
         pid_t pid = fork();
-        if (pid == -1) {
-            exit(EXIT_FAILURE);
-        }
+        check_error(pid, "fork");
         if (pid == 0) {
-            ex = f(i);
-            exit(ex);
+            int res = f(i);
+            exit(res);
         }
+    }
+
+    for (int i=0; i<n; ++i){
         int status;
-        pid = wait(&status);
+        pid_t pid = wait(&status);
         if (WIFEXITED(status))
             v[i] = WEXITSTATUS(status);
     }
+
 }
