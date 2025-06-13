42f39cf953a120946e9fe1d0f5840febd9ad2af3
Alessandro <alessandro.chitarrini51@gmail.com>
2025-06-13 12:36:07 +0200

    update e2A [07-2019]

--- src/2019/07/A/e2/e2A.c
+++ src/2019/07/A/e2/e2A.c
@@ -7,36 +7,34 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-void check_error(int res, char* msg) {
+void check_error(int res, char* msg){
     if (res != -1) return;
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
 int contains(char* s, char c, int n){
-    int i;
     int len = strlen(s);
     int m = len/n;
     int ex = 0;
-    if (m*n < len){
-        for (i=n*m; i<len; i++){
-            if (s[i] == c)
-                ex = 1;
+    if (m*n < len) {
+        for (int j = n*m; j < len; j++) {
+            if (s[j] == c)
+                return 1;
         }
     }
-    for (i=0; i<n; i++){
+    for (int i = 0; i < n; i++) {
         pid_t pid = fork();
-        if (pid == -1)
-            check_error(pid, "fork");
-                    if (pid==0){
-            for (int j = i*m;j<(i+1)*m;j++){
-                if (s[j] == c)
-                    ex = 1;
+        check_error(pid, "fork");
+        if (pid == 0) {
+            for (int k = i*m; k < (i+1)*m; k++) {
+                if (s[k] == c)
+                    exit(1);
             }
-        _exit(ex);
+            exit(0);
         }
     }
-    for (i=0; i<n; i++) {
+    for (int i = 0; i < n; i++) {
         int status;
         pid_t pid = wait(&status);
         check_error(pid, "wait");
@@ -45,4 +43,3 @@ int contains(char* s, char c, int n){
     }
     return ex;
 }
-
